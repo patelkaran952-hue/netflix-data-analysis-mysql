da16753c8103d0a817fd4d2204f0c67e8a288807
patelkaran952-hue <patelkaran952@gmail.com>
2026-03-31 19:49:02 +0530

    Add files via upload
    -Replaced empty values with null.
    -Fixed Date format
    
    Performed exploratory data analysis
    -Time Analysis
    -Percentage of movies and tv shows by year
    -Most common genres
    -Content growth trend

--- Project Netflix.sql
+++ Project Netflix.sql
@@ -120,6 +120,8 @@ order by duration desc;
 select distinct duration
 from netflix;
 
+
+
 -- Spliting duration into two columns beacuse of mixed datatype
 
 ALTER TABLE netflix ADD duration_length INT;
@@ -135,8 +137,138 @@ ALTER TABLE netflix DROP COLUMN duration;
 ALTER TABLE netflix
 RENAME COLUMN duration_int TO duration;
 
+
+-- Replacing empty values with null
+
+select *
+from netflix;
+
 select *
 from netflix
+where date_added = '';
+
+update netflix
+set director = null
+where director = '';
+
+update netflix
+set cast = null
+where cast = '';
+
+update netflix
+set rating = null
+where rating = '';
+
+update netflix
+set date_added = null
+where date_added = '';
+
+
+-- Fix Date Format
+
+UPDATE netflix
+SET date_added = STR_TO_DATE(date_added, '%M %d, %Y');
+
+ALTER TABLE netflix
+MODIFY COLUMN date_added DATE;
+
+
+
+-- Exploratory Data Analysis
+
+-- Time Analysis
+SELECT YEAR(date_added), COUNT(*)
+FROM netflix
+GROUP BY YEAR(date_added)
+ORDER BY 2 desc;
+
+SELECT country, COUNT(*) as total
+FROM netflix
+GROUP BY country
+ORDER BY total DESC
+limit 10;
+
+SELECT listed_in, COUNT(*)
+FROM netflix
+GROUP BY listed_in
+ORDER BY 2 DESC
+LIMIT 10;
+
+SELECT cast, COUNT(*) 
+FROM netflix
+GROUP BY cast
+ORDER BY 2 DESC
+LIMIT 10;
+
+
+-- Content added every month
+SELECT YEAR(date_added), MONTH(date_added), type, COUNT(*)
+FROM netflix
+GROUP BY YEAR(date_added), MONTH(date_added), type;
+
+-- Movies longer than 2 hours
+SELECT title, duration
+FROM netflix
+WHERE duration_type = 'min' AND duration > 120
+ORDER BY duration desc;
+
+-- TV Shows longer than 5 seasons 
+SELECT title, duration, duration_type
+FROM netflix
+WHERE duration_type = 'Seasons' AND duration > 5
+ORDER BY duration desc;
+
+
+-- Percentage of Movies vs TV Shows
+SELECT 
+type,
+COUNT(*) * 100.0 / (SELECT COUNT(*) FROM netflix) AS percentage
+FROM netflix
+GROUP BY type;
+
+-- Percentage of Movies vs TV Shows by year
+SELECT 
+YEAR(date_added) AS year,
+type,
+COUNT(*) * 100.0 / SUM(COUNT(*)) OVER (PARTITION BY YEAR(date_added)) AS percentage
+FROM netflix
+GROUP BY year, type
+ORDER BY year desc, type;
+
+
+-- Most common genres
+SELECT listed_in, COUNT(*)
+FROM netflix
+GROUP BY listed_in
+ORDER BY 2 DESC
+LIMIT 10;
+
+
+-- Content growth trend
+SELECT year(date_added), COUNT(*)
+FROM netflix
+GROUP BY year(date_added)
+ORDER BY year(date_added);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
