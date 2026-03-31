79b09185475b4d7f5d603ad35cf2b9e1fcb42acb
patelkaran952-hue <patelkaran952@gmail.com>
2026-03-31 12:58:20 +0530

    Add files via upload
    Removed duplicate values. The rest of the data cleaning is in progress.

--- Project Netflix.sql
+++ Project Netflix.sql
@@ -37,7 +37,113 @@ select *
 from netflix_titles;
 
 select * 
+from netflix;
+
+
+-- Remove duplicates
+
+select *,
+row_number() over(partition by `type`, title, director, cast, release_year) as row_num
+from netflix;
+
+with duplicate_cte as 
+(
+select *,
+row_number() over(partition by `type`, title, director, cast, release_year) as row_num
 from netflix
+)
+select * 
+from duplicate_cte
+where row_num > 1;
+
+select * 
+from netflix
+where title = 'Esperando la carroza';
+
+delete
+from netflix
+where show_id = 's304';
+
+
+-- Standardise Data
+
+select type, count(*) as total_content
+from netflix
+group by type;
+
+select country, count(*) as total_country
+from netflix
+group by country
+order by total_country desc;
+
+update netflix
+set country = "Undisclosed Location"
+where country = '';
+
+select*
+from netflix;
+
+select listed_in, count(*) as total_listed
+from netflix
+group by listed_in
+order by total_listed desc;
+
+select type, rating, count(*) as total_rating
+from netflix
+group by type, rating
+order by total_rating desc;
+
+select distinct rating
+from netflix;
+
+select *
+from netflix 
+where rating = '66 min';
+
+update netflix
+set duration = '66 min'
+where rating = '66 min';
+
+update netflix
+set rating = ''
+where rating = '66 min';
+
+select listed_in, count(*) as total_listed
+from netflix
+group by listed_in
+order by total_listed desc;
+
+select title, duration, type
+from netflix
+order by duration desc;
+
+select distinct duration
+from netflix;
+
+-- Spliting duration into two columns beacuse of mixed datatype
+
+ALTER TABLE netflix ADD duration_length INT;
+ALTER TABLE netflix ADD duration_type VARCHAR(20);
+
+UPDATE netflix
+SET 
+duration_length = CAST(SUBSTRING_INDEX(duration, ' ', 1) AS UNSIGNED),
+duration_type = SUBSTRING_INDEX(duration, ' ', -1);
+
+ALTER TABLE netflix DROP COLUMN duration;
+
+ALTER TABLE netflix
+RENAME COLUMN duration_int TO duration;
+
+select *
+from netflix
+
+
+
+
+
+
+
 
 
 
